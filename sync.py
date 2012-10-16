6fede237eb5ca256d1fd38ab0b792eabd29470cc
Magnus Ahltorp <map@kth.se>
2012-10-16 16:23:58 +0100

    Move resolution to syncutil

--- sync.py
+++ sync.py
@@ -10,7 +10,7 @@ import sys
 
 from util import *
 from afsutil import *
-from syncutil import dosync
+from syncutil import dosync, readbasefiles, getbasefilesdict
 
 if len(sys.argv) != 2:
     print >> sys.stderr, "usage:", sys.argv[0], "<local directory>"
@@ -52,23 +52,7 @@ status = fs.getstatusforfids([e["fid"] for e in serverfiles])
 for e in serverfiles:
     e["status"] = status[e["fid"]]
 
-
-basefiles = []
-basefilesdict = {}
-
-def readbasefiles():
-    cursor.execute('select vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum from files where deleted is null')
-    global basefiles
-    basefiles = [dict([(k, row[k]) for k in row.keys()]) for row in cursor.fetchall()]
-
-    # print
-    # print "base"
-    global basefilesdict
-    basefilesdict = dict([(e["path"], e) for e in basefiles])
-    # print basefilesdict.keys()
-
-readbasefiles()
-
+readbasefiles(cursor)
 
 localfiles = [e for e in getlocalfiles(basepath) if not e["path"].startswith("/.afssync/")]
 #print localfiles
@@ -84,7 +68,7 @@ localfilesdict = dict([(e["path"], e) for e in localfiles])
 serverfilesdict = dict([(e["path"], e) for e in serverfiles])
 #print serverfilesdict.keys()
 
-allfiles = sorted(set(localfilesdict.keys()) | set(basefilesdict.keys()) | set(serverfilesdict.keys()))
+allfiles = sorted(set(localfilesdict.keys()) | set(getbasefilesdict().keys()) | set(serverfilesdict.keys()))
 
 def fetchfile(basepath, serverfile):
     (fd, name) = tempfile.mkstemp(dir=basepath+"/.afssync/fetched")
@@ -98,83 +82,10 @@ def fetchfile(basepath, serverfile):
     status["checksum"] = checksum
     return (name, status)
 
-def sendfileandupdate(basepath, name, localfile, parentfid, serverfile=None):
-    # XXX creates and uploads directly to final filename.
-    #   advantage: atomically ensures that no file is overwritten
-    #   disadvantage: file is visible to other clients directly
-    print "send file", name
-    localfiletype = mode_to_filetype(localfile["mode"])
-    if localfiletype == 1:
-        if serverfile == None:
-            result = fs.fsconn.create(fsconn, parentfid, localfile["name"], localfile["mode"] & 0777, localfile["mtime"])
-            fid = result["fid"]
-        else:
-            fid = serverfile["fid"]
-        contents = readwholefile(basepath + "/" + name)
-        status = arlalow.storewholefile(fs.fsconn, fid, contents, localfile["mode"] & 0777, localfile["mtime"])
-        (volume, vnode, unique) = fid
-        checksum = checksumcontents(contents)
-        cursor.execute('delete from files where path = ?', (name,))
-        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', (vnode, unique, localfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"], status["length"], checksum))
-        conn.commit()
-        if not serverfile:
-            print "created file with fid", fid
-    elif localfiletype == 2:
-        print localfile
-        result = arlalow.mkdir(fs.fsconn, parentfid, localfile["name"], localfile["mode"] & 0777, localfile["mtime"])
-        fid = result["fid"]
-        status = result["status"]
-        (volume, vnode, unique) = fid
-        cursor.execute('delete from files where path = ?', (name,))
-        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime) values (?, ?, ?, ?, ?, ?, ?)', (vnode, unique, localfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"]))
-        conn.commit()
-        print "created directory with fid", fid
-    else:
-        raise Exception("Cannot handle symlinks yet")
-
-def fetchfileandupdate(basepath, name, serverfile):
-    serverfiletype = serverfile["status"]["type"]
-    if serverfiletype == 1:
-        (tempname, status) = fetchfile(basepath, serverfile)
-        os.chmod(tempname, status["mode"] & 0777)
-        os.utime(tempname, (status["modtime"], status["modtime"]))
-        os.rename(tempname, basepath + "/" + name)
-        (volume, vnode, unique) = serverfile["fid"]
-        cursor.execute('delete from files where path = ?', (name,))
-        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', (vnode, unique, serverfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"], status["length"], status["checksum"]))
-        conn.commit()
-    elif serverfiletype == 2:
-        # XXX doesn't update basefiles list
-        status = serverfile["status"]
-        (volume, vnode, unique) = serverfile["fid"]
-        os.mkdir(basepath + "/" + name)
-        os.chmod(basepath + "/" + name, status["mode"] & 0777)
-        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime) values (?, ?, ?, ?, ?, ?, ?)', (vnode, unique, serverfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"]))
-        conn.commit()
-    else:
-        raise Exception("Cannot handle symlinks yet")
-        
-
-def conflictresolution(basepath, name, localfile, serverfile):
-    # XXX doesn't restart the syncing, one file should be sent and one file should be fetched
-    localfiletype = mode_to_filetype(localfile["mode"])
-    serverfiletype = serverfile["status"]["type"]
-    print "resolve file", name, localfiletype, serverfiletype
-    if serverfiletype == 1:
-        #(tempname, status) = fetchfile(basepath, serverfile)
-        if localfile["mtime"] > serverfile["status"]["modtime"]:
-            # XXX check that new name is unused
-            arlalow.rename(fs.fsconn, serverfile["parent"], serverfile["name"], serverfile["parent"], serverfile["name"] + ";serverrenamed-1")
-        else:
-            os.rename(basepath + "/" + name, basepath + "/" + name + ";locallyrenamed-1")
-    elif serverfiletype == 2:
-        raise Exception("Cannot handle directories yet")
-    else:
-        raise Exception("Cannot handle symlinks yet")
 
 
 try:
-    dosync(allfiles, fs, localfilesdict, serverfilesdict, basefilesdict, basepath)
+    dosync(conn, cursor, allfiles, fs, localfilesdict, serverfilesdict, getbasefilesdict(), basepath)
 finally:
     arlalow.giveupallcallbacks(fs.fsconn)
 
--- syncutil.py
+++ syncutil.py
@@ -1,16 +1,108 @@
 import os
 import stat
+import sys
 from util import *
 from afsutil import *
 
-def synconefile(basepath, name, localfile, serverfile, basefile, parentfid):
+basefiles = []
+basefilesdict = {}
+
+def readbasefiles(cursor):
+    cursor.execute('select vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum from files where deleted is null')
+    global basefiles
+    basefiles = [dict([(k, row[k]) for k in row.keys()]) for row in cursor.fetchall()]
+
+    # print
+    # print "base"
+    global basefilesdict
+    basefilesdict = dict([(e["path"], e) for e in basefiles])
+    # print basefilesdict.keys()
+
+def getbasefilesdict():
+    return basefilesdict
+
+def sendfileandupdate(conn, cursor, fs, basepath, name, localfile, parentfid, serverfile=None):
+    # XXX creates and uploads directly to final filename.
+    #   advantage: atomically ensures that no file is overwritten
+    #   disadvantage: file is visible to other clients directly
+    print "send file", name
+    localfiletype = mode_to_filetype(localfile["mode"])
+    if localfiletype == 1:
+        if serverfile == None:
+            result = fs.fsconn.create(fsconn, parentfid, localfile["name"], localfile["mode"] & 0777, localfile["mtime"])
+            fid = result["fid"]
+        else:
+            fid = serverfile["fid"]
+        contents = readwholefile(basepath + "/" + name)
+        status = arlalow.storewholefile(fs.fsconn, fid, contents, localfile["mode"] & 0777, localfile["mtime"])
+        (volume, vnode, unique) = fid
+        checksum = checksumcontents(contents)
+        cursor.execute('delete from files where path = ?', (name,))
+        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', (vnode, unique, localfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"], status["length"], checksum))
+        conn.commit()
+        if not serverfile:
+            print "created file with fid", fid
+    elif localfiletype == 2:
+        print localfile
+        result = arlalow.mkdir(fs.fsconn, parentfid, localfile["name"], localfile["mode"] & 0777, localfile["mtime"])
+        fid = result["fid"]
+        status = result["status"]
+        (volume, vnode, unique) = fid
+        cursor.execute('delete from files where path = ?', (name,))
+        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime) values (?, ?, ?, ?, ?, ?, ?)', (vnode, unique, localfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"]))
+        conn.commit()
+        print "created directory with fid", fid
+    else:
+        raise Exception("Cannot handle symlinks yet")
+
+def conflictresolution(fs, basepath, name, localfile, serverfile):
+    # XXX doesn't restart the syncing, one file should be sent and one file should be fetched
+    localfiletype = mode_to_filetype(localfile["mode"])
+    serverfiletype = serverfile["status"]["type"]
+    print "resolve file", name, localfiletype, serverfiletype
+    name = name.rstrip("/")
+    if serverfiletype in [1, 2, 3]:
+        #(tempname, status) = fetchfile(basepath, serverfile)
+        if localfile["mtime"] > serverfile["status"]["modtime"]:
+            # XXX check that new name is unused
+            arlalow.rename(fs.fsconn, serverfile["parent"], serverfile["name"], serverfile["parent"], serverfile["name"] + ";serverrenamed-1")
+        else:
+            os.rename(basepath + "/" + name, basepath + "/" + name + ";locallyrenamed-1")
+    elif serverfiletype == 2:
+        raise Exception("Cannot handle directories yet")
+    else:
+        raise Exception("Cannot handle symlinks yet")
+
+def fetchfileandupdate(conn, cursor, basepath, name, serverfile):
+    serverfiletype = serverfile["status"]["type"]
+    if serverfiletype == 1:
+        (tempname, status) = fetchfile(basepath, serverfile)
+        os.chmod(tempname, status["mode"] & 0777)
+        os.utime(tempname, (status["modtime"], status["modtime"]))
+        os.rename(tempname, basepath + "/" + name)
+        (volume, vnode, unique) = serverfile["fid"]
+        cursor.execute('delete from files where path = ?', (name,))
+        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime, length, checksum) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', (vnode, unique, serverfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"], status["length"], status["checksum"]))
+        conn.commit()
+    elif serverfiletype == 2:
+        # XXX doesn't update basefiles list
+        status = serverfile["status"]
+        (volume, vnode, unique) = serverfile["fid"]
+        os.mkdir(basepath + "/" + name)
+        os.chmod(basepath + "/" + name, status["mode"] & 0777)
+        cursor.execute('insert into files (vnode, uniq, filetype, dataversion, path, mode, localmodtime) values (?, ?, ?, ?, ?, ?, ?)', (vnode, unique, serverfiletype, status["dataversion"], name, status["mode"] & 0777, status["modtime"]))
+        conn.commit()
+    else:
+        raise Exception("Cannot handle symlinks yet")
+        
+def synconefile(conn, cursor, fs, basepath, name, localfile, serverfile, basefile, parentfid):
     localchecksum = checksumfile(basepath + "/" + name) if localfile and stat.S_ISREG(localfile["mode"]) else None
     places = (localfile != None, basefile != None, serverfile != None)
     
     if places == (False, False, True):
         print name, "new on server"
-        fetchfileandupdate(basepath, name, serverfile)
-        readbasefiles()
+        fetchfileandupdate(conn, cursor, basepath, name, serverfile)
+        readbasefiles(cursor)
 #    elif places == (False, True, False):
 #        pass
     elif places == (False, True, True):
@@ -18,23 +110,23 @@ def synconefile(basepath, name, localfile, serverfile, basefile, parentfid):
             arlalow.remove(fs.fsconn, serverfile["parent"], serverfile["name"])
             cursor.execute('delete from files where path = ?', (name,))
             conn.commit()
-            readbasefiles()
+            readbasefiles(cursor)
         elif basefile["filetype"] == 2:
             def removedir():
                 arlalow.rmdir(fs.fsconn, serverfile["parent"], serverfile["name"])
                 cursor.execute('delete from files where path = ?', (name,))
                 conn.commit()
-                readbasefiles()
+                readbasefiles(cursor)
             return (lambda curpath: not curpath.startswith(name), removedir)
         else:
             raise Exception("Cannot handle symlinks yet")
     elif places == (True, False, False):
         print name, "created on client"
-        sendfileandupdate(basepath, name, localfile, parentfid)
-        readbasefiles()
+        sendfileandupdate(conn, cursor, fs, basepath, name, localfile, parentfid)
+        readbasefiles(cursor)
     elif places == (True, False, True):
         print name, "created on server and client"
-        conflictresolution(basepath, name, localfile, serverfile)
+        conflictresolution(fs, basepath, name, localfile, serverfile)
     elif places == (True, True, False):
         print name, "removed from server"
         basefiletype = basefile["filetype"]
@@ -42,13 +134,13 @@ def synconefile(basepath, name, localfile, serverfile, basefile, parentfid):
             os.rename(basepath + "/" + name, basepath+"/.afssync/trash/%d-%d" % (basefile["vnode"], basefile["uniq"]))
             cursor.execute("update files set deleted = 1 where path = ?", (name, ))
             conn.commit()
-            readbasefiles()
+            readbasefiles(cursor)
         elif basefiletype == 2:
             def removedir():
                 os.rmdir(basepath + "/" + name)
                 cursor.execute("delete from files where path = ?", (name, ))
                 conn.commit()
-                readbasefiles()
+                readbasefiles(cursor)
             return (lambda curpath: not curpath.startswith(name), removedir)
         else:
             raise Exception("Cannot handle symlinks yet")
@@ -61,11 +153,11 @@ def synconefile(basepath, name, localfile, serverfile, basefile, parentfid):
             if changes == (False, False):
                 pass
             elif changes == (False, True):
-                fetchfileandupdate(basepath, name, serverfile)
-                readbasefiles()
+                fetchfileandupdate(conn, cursor, basepath, name, serverfile)
+                readbasefiles(cursor)
             elif changes == (True, False):
                 sendfileandupdate(basepath, name, localfile, parentfid, serverfile=serverfile)
-                readbasefiles()
+                readbasefiles(cursor)
             else:
                 raise Exception(name + ": Unhandled changes state " + str(changes))
         elif basefile["filetype"] == 2:
@@ -77,7 +169,7 @@ def synconefile(basepath, name, localfile, serverfile, basefile, parentfid):
     else:
         raise Exception("Unhandled places state " + str(places))
 
-def dosync(allfiles, fs, localfilesdict, serverfilesdict, basefilesdict, basepath):
+def dosync(conn, cursor, allfiles, fs, localfilesdict, serverfilesdict, basefilesdict, basepath):
     delayedtasks = []
     for f in allfiles:
         for delayedtask in delayedtasks:
@@ -105,7 +197,7 @@ def dosync(allfiles, fs, localfilesdict, serverfilesdict, basefilesdict, basepat
         #    serverstat = status[serverfile["fid"]]
         #    print "server mtime", serverstat["modtime"], "mode %o" % serverstat["mode"], "size", serverstat["length"]
     
-        delayedtask = synconefile(basepath, f, localfile, serverfile, basefile, parentfid)
+        delayedtask = synconefile(conn, cursor, fs, basepath, f, localfile, serverfile, basefile, parentfid)
         if delayedtask:
             delayedtasks[0:0] = [delayedtask]
         #    break
